74645c6b509304b8791bd3add6f14056ce9c6db7
Garosan <garosanchez27@gmail.com>
2024-11-28 01:28:55 -0600

    Fix testfile issues by adding eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true, // Enable Node.js global variables and scoping
+    es6: true, // Enable ES6 features
+    mocha: true, // Enable Mocha global variables for testing
+  },
+  parserOptions: {
+    ecmaVersion: 2021, // Specify the version of ECMAScript syntax you want to use
+    sourceType: "module", // Enable ES6 modules
+  },
+  rules: {
+    // Add or customize rules here
+  },
+};
